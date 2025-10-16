0be7ea56bfbe6d99e9aa39feda605e7a4e95770d
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-16 19:59:39 +0600

    mongodb conected succesfully in server

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 const app = express(); 
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const { MongoClient, ObjectId } = require('mongodb');
 // middleware
 app.use(cors())
 app.use(express.json())
+// mongodb connection
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr8kgxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri);
 
 app.get('/', (req, res)=>{
     res.send("TaskHive Make There Server Successfully Updated Now")
